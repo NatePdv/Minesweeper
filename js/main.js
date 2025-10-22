00a540732f4be632ec4a87bffee47781e8d62743
NatePdv <143347491+NatePdv@users.noreply.github.com>
2025-10-22 15:18:16 +0300

    App skeleton

---
+++ js/main.js
@@ -0,0 +1,41 @@
+'use strict'
+var gBoard = {
+    minesAroundCount: 4,
+    isRevealed: false,
+    isMine: false,
+    isMarked: false
+}
+var gLevel = {
+    SIZE: 4,
+    MINES: 2
+}
+var gGame = {
+    isOn: false,
+    revealedCount: 0,
+    markedCount: 0,
+    secsPassed: 0
+}
+function onInit() {
+
+}
+function buildBoard() {
+
+}
+function setMinesNegsCount(board) {
+
+}
+function renderBoard() {
+
+}
+function onCellClicked(elCell, i, j) {
+
+}
+function onCellMarked(elCell, i, j) {
+
+}
+function checkGameOver() {
+
+}
+function expandReveal(board, elCell, i, j) { 
+    
+}
